974d64c97a7b9909fa061b23c5bf5fd6570512df
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-07-10 16:48:53 +0530

    redis email included

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 # @Author: prabhakar
 # @Date:   2016-07-09 21:06:19
-# @Last Modified by:   prabhakar
-# @Last Modified time: 2016-07-10 00:38:27
+# @Last Modified by:   Prabhakar Gupta
+# @Last Modified time: 2016-07-10 16:44:17
 
 import requests.packages.urllib3
 requests.packages.urllib3.disable_warnings()
 
+import redis
 from flask import Flask, abort, request
 from flask.ext.cors import CORS
 
@@ -21,10 +22,18 @@ def main():
         abort(404)
     else :
         github_username = request.form.get('username').encode('ascii', 'ignore')
-        x = github_email.get(github_username)
+        red = redis.StrictRedis(host='localhost', port=6379, db=0)
+        red_email = red.hget('email_list', github_username)
+
+        if red_email:
+            return "{0}".format(red_email)
         
+        x = github_email.get(github_username)
         if x['success']:
-            return "{0}".format(x['email'][0])
+            email = x['email'][0]
+            
+            red.hset('email_list', github_username, email)
+            return "{0}".format(email)
         else:
             abort(404)
 
