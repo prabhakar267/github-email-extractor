de1a6a74b4e276c4ddaabd36a1a3d884974b54a9
BigGuy573 <48465089+BigGuy573@users.noreply.github.com>
2021-07-22 21:59:20 -0500

    Update and rename manifest.json to manifest.java

---
+++ src/manifest.java
@@ -0,0 +1,30 @@
+@@ -1,4 +1,40 @@
+{
+{@@ -0,0 +1,35 @@
+base.java/BigGuy573-main.prettierrc.java 
+'@'
+master-w3c-spec-generator 
+'@' '.prettierrc' '@' '#0.01' 
+ prettierrc.java 
+ '@'
+ +1,18{->
+'->}'```"---"```'{-> 
+'->}'#'{->
+ '->}'#$
+  '->}'##
+   '->}'##$'{'#'->}
+   ```"---"```
+   ->{'```}
+   '```"---"```'#'
+   singleQuote: 
+   exception
+   "```---```{
+  '}'->{''#$"("
+   exception-"```---```
+  "singleQuote-exception={->
+  '}->']```---```->{
+  '}'
+  ##[example=next-two-lines'
+  '('#''#)
+  #'{'${->
+  '##$->}'##$(singleQuote='##$'=#0{->
